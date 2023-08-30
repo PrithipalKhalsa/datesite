1d7193c4ca7decf976d5f56adca6edb2b877b9de
Prith <prithipalpsk@gmail.com>
2023-08-29 20:13:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function loadJSON(url) {
 for (let i = 0; i < outputElements.length; i++) {
     const button = document.getElementById('button' + (i + 1));
     button.addEventListener('click', async () => {
-        const textArray = await loadJSON('text' + (i + 1) + '.json');
+        const textArray = await loadJSON('array' + (i + 1) + '.json');
         const randomIndex = Math.floor(Math.random() * textArray.length);
         outputElements[i].textContent = textArray[randomIndex];
     });
