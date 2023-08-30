c1a8b8a26d08285ce8203affb62bf2504dc82242
Prith <prithipalpsk@gmail.com>
2023-08-29 21:12:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,22 +12,11 @@ async function loadJSON(url) {
 }
 
 // Event listeners for buttons
-// Modify the event listeners in your script.js as follows
-
 for (let i = 0; i < outputElements.length; i++) {
     const button = document.getElementById('button' + (i + 1));
     button.addEventListener('click', async () => {
         const textArray = await loadJSON('text' + (i + 1) + '.json');
         const randomIndex = Math.floor(Math.random() * textArray.length);
-
-        // Apply the roll-text class before changing the content
-        outputElements[i].classList.add('roll-text');
-
-        // Set the new content after a delay to allow animation
-        setTimeout(() => {
-            outputElements[i].textContent = textArray[randomIndex];
-            // Remove the roll-text class after the animation
-            outputElements[i].classList.remove('roll-text');
-        }, 1000); // Adjust the delay as needed
+        outputElements[i].textContent = textArray[randomIndex];
     });
 }
