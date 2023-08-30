8c37e0e17c7afe1b251ff8768e491d0daad8c360
Prith <prithipalpsk@gmail.com>
2023-08-29 21:01:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,28 @@ async function loadJSON(url) {
 for (let i = 0; i < outputElements.length; i++) {
     const button = document.getElementById('button' + (i + 1));
     button.addEventListener('click', async () => {
-        const textArray = await loadJSON('array' + (i + 1) + '.json');
+        const textArray = await loadJSON('text' + (i + 1) + '.json');
         const randomIndex = Math.floor(Math.random() * textArray.length);
-        outputElements[i].textContent = textArray[randomIndex];
+
+        // Apply the roll-text class before changing the content
+        outputElements[i].classList.add('roll-text');
+
+        // Set the new content after animation completes
+        setTimeout(() => {
+            // Remove the roll-text class to stop animation
+            outputElements[i].classList.remove('roll-text');
+
+            // Roll a second time with a longer animation
+            setTimeout(() => {
+                outputElements[i].classList.add('roll-text');
+                setTimeout(() => {
+                    outputElements[i].classList.remove('roll-text');
+                    // Set the final content after the animation
+                    setTimeout(() => {
+                        outputElements[i].textContent = textArray[randomIndex];
+                    }, 200); // Adjust the delay as needed
+                }, 1500); // Adjust the delay as needed
+            }, 500); // Adjust the delay as needed
+        }, 1000); // Adjust the delay as needed
     });
 }
